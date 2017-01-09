e7860dfb3e86432fee4f189cbde84be261ea5124
rahul_baruri <rahul.baruri@gagagugu.com>
2017-01-09 14:23:26 +0600

    Mezor changed and indentation added

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,58 +1,56 @@
 function selectEmails(keywords) {
     var newKeyWordsWithCount = keywords;
-    if( document.getElementsByClassName("F").length) {
- document.getElementsByClassName("F")[0].querySelectorAll("tbody>tr").forEach(function(node){
-      var columns = node.querySelectorAll("td");
-      var subject= columns[3].querySelector("span").textContent;
-       var description= columns[5].querySelectorAll("span")[0].textContent;
-       //TODO: NEED TO calculate more perfect count regarding titles because on title may have multiple common keywords 
-        newKeyWordsWithCount.forEach(function(keyWord,index) {
-          if((subject.indexOf(keyWord.title)> -1 || description.indexOf(keyWord.title) > -1) && keyWord.enabled === true ){
-           newKeyWordsWithCount[index].totalMatched = newKeyWordsWithCount[index].totalMatched +1 ;
-           columns[1].querySelector(".oZ-jc").click();
-            }
-         });
+    if (document.getElementsByClassName("F").length) {
+        document.getElementsByClassName("F")[0].querySelectorAll("tbody>tr").forEach(function (node) {
+            var columns = node.querySelectorAll("td");
+            var subject = columns[3].querySelector("span").textContent;
+            var description = columns[5].querySelectorAll("span")[0].textContent;
+            //TODO: NEED TO calculate more perfect count regarding titles because on title may have multiple common keywords 
+            newKeyWordsWithCount.forEach(function (keyWord, index) {
+                if ((subject.indexOf(keyWord.title) > -1 || description.indexOf(keyWord.title) > -1) && keyWord.enabled === true) {
+                    newKeyWordsWithCount[index].totalMatched = newKeyWordsWithCount[index].totalMatched + 1;
+                    columns[1].querySelector(".oZ-jc").click();
+                }
+            });
 
-    });
-     return newKeyWordsWithCount;
+        });
+        return newKeyWordsWithCount;
 
     } else {
         console.error("Not Gmail Page");
         return null;
     }
-   
+
 }
 
 
 function deleteMails() {
-    var deleteBtn ;
-    if(window.location.href.indexOf("#spam") > -1) {
-           deleteBtn = document.querySelector(".aFi");
-    }else {
-          deleteBtn = document.querySelector(".T-I-Js-Gs");
+    var deleteBtn;
+    if (window.location.href.indexOf("#spam") > -1) {
+        deleteBtn = document.querySelector(".aFi");
+    } else {
+        deleteBtn = document.querySelector(".T-I-Js-Gs");
     }
-      
-        var down = new MouseEvent('mousedown');
-        var up = new MouseEvent('mouseup');
-              deleteBtn.dispatchEvent(down);
-              deleteBtn.dispatchEvent(up);
+
+    var down = new MouseEvent('mousedown');
+    var up = new MouseEvent('mouseup');
+    deleteBtn.dispatchEvent(down);
+    deleteBtn.dispatchEvent(up);
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    
-
-    switch(request.command) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    switch (request.command) {
         case "selectMail":
-                var newkeywords = selectEmails(request.keywords);
-                if(newkeywords) {
-                sendResponse({result: "selection_success",keywordItems:newkeywords});
-                }
-
-                break;
+            var newkeywords = selectEmails(request.keywords);
+            if (newkeywords) {
+                sendResponse({ result: "selection_success", keywordItems: newkeywords });
+            }
+            break;
+            
         case "deleteMails":
-                    deleteMails();
-                      sendResponse({result:'deleted'});
-                    break;       
+            deleteMails();
+            sendResponse({ result: 'deleted' });
+            break;
     }
 });
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,147 +1,144 @@
-chrome.tabs.getSelected(null, function(tab) {
-          window.tabUrl = tab.url;
-        });
+chrome.tabs.getSelected(null, function (tab) {
+  window.tabUrl = tab.url;
+});
 
-var app = angular.module("emailDestoryer",[]);
+var app = angular.module("emailDestoryer", []);
 
- app.controller("EmailDestroyerCtlr",['$scope','$timeout','storageService',function ($scope,$timeout,storageService){
+app.controller("EmailDestroyerCtlr", ['$scope', '$timeout', 'storageService', function ($scope, $timeout, storageService) {
 
- $scope.gmailTab = false;
- $scope.deleting = false;
- $scope.totalMails = 0;
-  $scope.keywords =[
+  $scope.gmailTab = false;
+  $scope.deleting = false;
+  $scope.totalMails = 0;
+  $scope.keywords = [
     {
-    title:'Slack',
-    enabled: true,
-    totalMatched: 0   
-  }
+      title: 'Slack',
+      enabled: true,
+      totalMatched: 0
+    }
   ];
-    $scope.keywords = storageService.getAllKeywords();
-   
-   $scope.newKeyWord = {
-     title: '',
+  $scope.keywords = storageService.getAllKeywords();
+
+  $scope.newKeyWord = {
+    title: '',
     enabled: true,
-    totalMatched: 0   
-   };
+    totalMatched: 0
+  };
 
-   $scope.submitForm = function(event) {
-       if(event.keyCode === 13) {  
-         $scope.keywords.unshift($scope.newKeyWord);
-         var keywordAdded = storageService.addNewKeyword($scope.newKeyWord);
-         if(keywordAdded) {
-           console.info('key word added');
-         } else {
-            console.error('something went wrong while adding keyword');
-         }
-         $scope._resetForm();
-     }
-   };
+  $scope.submitForm = function (event) {
+    if (event.keyCode === 13) {
+      $scope.keywords.unshift($scope.newKeyWord);
+      var keywordAdded = storageService.addNewKeyword($scope.newKeyWord);
+      if (keywordAdded) {
+        console.info('key word added');
+      } else {
+        console.error('something went wrong while adding keyword');
+      }
+      $scope._resetForm();
+    }
+  };
 
-   $scope._resetForm= function(){
-     $scope.newKeyWord = '';
-   };
-   
-   $scope.deleteKeyword = function(index) {
-     //e.preventDefault();
-     var confirmed = confirm("Do you really want to delete this keyword?");
-     if(confirmed) {
-       $scope.keywords.splice([index],1);
-       var keywordDeleted = storageService.deleteKeyWord(index);
-         if(keywordDeleted) {
-           console.info('key word deleted successfully');
-         } else {
-            console.error('something went wrong while deleting keyword');
-         }
-     }
-   };
-   
-    
-   $scope.changeStatusKeyword = function(index) {
-       $scope.keywords[index].enabled =!$scope.keywords[index].enabled;
-   var keywordStatusChanged = storageService.changeKeywordStatus(index);
-         if(keywordStatusChanged) {
-           console.info('keyword status changed successfully');
-         } else {
-            console.error('something went wrong while changing status of keyword');
-         }
-   
-   };
-   
-   $scope.mailFound = function(){
-     var mails = $scope.keywords.map(function(word) {
-       return word.totalMatched;
-     });
-     $scope.totalMails = mails.reduce(function(first,second) {
-       return first+second;
-     });
-     console.info("Total mail"+$scope.totalMails);
-   }
-    
-   
-$scope.selectEmails = function(tab) 
-        {
-          console.info("Initiated");
-         
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {command: "selectMail",keywords:storageService.getAllKeywords()}, function(response) {
-            if(response && response.hasOwnProperty('result') && response.result=='selection_success' && response.hasOwnProperty('keywordItems')) {
-              $scope.$apply(function($scope){
-                $scope.keywords = response.keywordItems;
-                                  var mails = $scope.keywords.map(function(word) {
-                                 return word.totalMatched;
-                           });
-                    $scope.totalMails = mails.reduce(function(first,second) {
-                   return first+second;
-                });
-     console.info("Total mail"+$scope.totalMails);
-              })
-            } else {
+  $scope._resetForm = function () {
+    $scope.newKeyWord = '';
+  };
 
-            }
-        });
+  $scope.deleteKeyword = function (index) {
+    //e.preventDefault();
+    var confirmed = confirm("Do you really want to delete this keyword?");
+    if (confirmed) {
+      $scope.keywords.splice([index], 1);
+      var keywordDeleted = storageService.deleteKeyWord(index);
+      if (keywordDeleted) {
+        console.info('key word deleted successfully');
+      } else {
+        console.error('something went wrong while deleting keyword');
+      }
+    }
+  };
+
+
+  $scope.changeStatusKeyword = function (index) {
+    $scope.keywords[index].enabled = !$scope.keywords[index].enabled;
+    var keywordStatusChanged = storageService.changeKeywordStatus(index);
+    if (keywordStatusChanged) {
+      console.info('keyword status changed successfully');
+    } else {
+      console.error('something went wrong while changing status of keyword');
+    }
+
+  };
+
+  $scope.mailFound = function () {
+    var mails = $scope.keywords.map(function (word) {
+      return word.totalMatched;
+    });
+    $scope.totalMails = mails.reduce(function (first, second) {
+      return first + second;
+    });
+    console.info("Total mail" + $scope.totalMails);
+  }
+
+
+  $scope.selectEmails = function (tab) {
+    console.info("Initiated");
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { command: "selectMail", keywords: storageService.getAllKeywords() }, function (response) {
+        if (response && response.hasOwnProperty('result') && response.result == 'selection_success' && response.hasOwnProperty('keywordItems')) {
+          $scope.$apply(function ($scope) {
+            $scope.keywords = response.keywordItems;
+            var mails = $scope.keywords.map(function (word) {
+              return word.totalMatched;
+            });
+            $scope.totalMails = mails.reduce(function (first, second) {
+              return first + second;
+            });
+            console.info("Total mail" + $scope.totalMails);
+          })
+        }
+      });
     });
 
-   };
+  };
 
 
-   $scope.deleteFilteredEmails = function() {
-      $scope.deleting = true;
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {command: "deleteMails",keywords:$scope.keywords}, function(response) {
-          
-            if(response && response.hasOwnProperty('result') && response.result==='deleted') {
-              $timeout(function(){
-                $scope.$apply(function($scope){
-                  $scope.deleting= false;
-                });
-              },3000);
-            }
-        });
+  $scope.deleteFilteredEmails = function () {
+    $scope.deleting = true;
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { command: "deleteMails", keywords: $scope.keywords }, function (response) {
+
+        if (response && response.hasOwnProperty('result') && response.result === 'deleted') {
+          $timeout(function () {
+            $scope.$apply(function ($scope) {
+              $scope.deleting = false;
+            });
+          }, 3000);
+        }
+      });
     });
-     
-   };
+
+  };
 
 
-// Called when the user clicks on the browser action.
+  // Called when the user clicks on the browser action.
 
 
-   $scope.getCurrentTabURL = function() {
-       chrome.tabs.getSelected(null, function(tab) {
-           if(window.tabUrl.indexOf("mail.google.com") > -1) {
-               console.log("Found : "+window.tabUrl);
-               $scope.$apply(function () {
-                   $scope.gmailTab = true;
-
-                  
-               });
-           }
-        })
-   };
-
-   $scope.getCurrentTabURL();
+  $scope.getCurrentTabURL = function () {
+    chrome.tabs.getSelected(null, function (tab) {
+      if (window.tabUrl.indexOf("mail.google.com") > -1) {
+        console.log("Found : " + window.tabUrl);
+        $scope.$apply(function () {
+          $scope.gmailTab = true;
 
 
+        });
+      }
+    })
+  };
 
-   
+  $scope.getCurrentTabURL();
+
+
+
+
 }]);
 
